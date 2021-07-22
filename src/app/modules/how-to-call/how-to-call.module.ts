d1b78cfc5d7654ca0496822b033b5d1b9f11c291
tarzan0420 <mikhaylov.ks0420@outlook.com>
2021-07-23 08:31:14 +0900

    Multi languages on HowToCall

--- src/app/modules/how-to-call/how-to-call.module.ts
+++ src/app/modules/how-to-call/how-to-call.module.ts
@@ -4,7 +4,8 @@ import { HowToCallComponent} from './page/how-to-call.component';
 import { SharedModule,ImportsMaterialModule} from '@/shared/shared.module';
 import {FormsModule} from '@angular/forms';
 import {PipeModule} from '@/_pipe/pipe/pipe.module';
-import { HowToCallRoutingModule} from './how-to-call-routing.module'
+import { HowToCallRoutingModule} from './how-to-call-routing.module';
+import { TranslateModule } from '@ngx-translate/core';
 @NgModule({
   declarations: [
   	HowToCallComponent
@@ -15,7 +16,8 @@ import { HowToCallRoutingModule} from './how-to-call-routing.module'
     SharedModule,
     CommonModule,
     FormsModule,
-    HowToCallRoutingModule
+    HowToCallRoutingModule,
+    TranslateModule
   ]
 })
 export class HowToCallModule { }
